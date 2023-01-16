6b740e47978c4b70fe730458bb84674a644474fa
zaynbaig21 <119931732+zaynbaig21@users.noreply.github.com>
2023-01-16 13:20:51 +0000

    date v1

--- src/date.py
+++ src/date.py
@@ -1,19 +1,19 @@
-import read_file
-import re
-from datetime import datetime
+import read_file 
+import re 
+from datetime import datetime 
   
-# initializing string
-test_str = "gfg at 2021-01-04"
+# initializing string 
+test_str = "gfg at 2021-01-04" 
   
-# printing original string
-print("The original string is : " + str(test_str))
+# printing original string 
+print("The original string is : " + str(test_str)) 
   
-# searching string
-match_str = re.search(r'\d{4}-\d{2}-\d{2}', test_str)
+# searching string 
+match_str = re.search(r'\d{4}-\d{2}-\d{2}', test_str) 
   
-# computed date
-# feeding format
-res = datetime.strptime(match_str.group(), '%Y-%m-%d').date()
+# computed date 
+# feeding format 
+res = datetime.strptime(match_str.group(), '%Y-%m-%d').date() 
   
-# printing result
-print("Computed date : " + str(res))
+# printing result 
+print("Computed date : " + str(res)) 
