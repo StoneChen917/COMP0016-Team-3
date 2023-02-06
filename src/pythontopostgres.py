708c297f42b424621f1b1be8b7d6577bebbf4588
StoneChen917 <zcabhc0@ucl.ac.uk>
2023-02-06 13:56:35 +0000

    ui&postgres

---
+++ src/pythontopostgres.py
@@ -0,0 +1,34 @@
+import psycopg2
+
+
+
+hostname = "localhost"
+database = "postgres"
+username = "postgres"
+pwd = "Abc123456"
+port_id = 5433
+conn = None
+cur = None
+
+try:
+    conn = psycopg2.connect(
+                host = hostname,
+                dbname = database,
+                user = username,
+                password = pwd,
+                port = port_id)
+
+    cur = conn.cursor()
+
+    create_script = ''' CREATE TABLE employee (
+                            id      int PRIMARY KEY,
+                            name    varchar(40) NOT NULL)'''
+
+
+except Exception as error:
+    print(error)
+finally:
+    if cur is not None:
+        cur.close()
+    if cur is not None:
+        conn.close()
--- src/ui.py
+++ src/ui.py
@@ -57,36 +57,58 @@ class MainPage(Frame):
 
         self.file_text = ""
 
-        img = ImageTk.PhotoImage(Image.open("src/Assets/Images/IFRC-logo.png").resize((110, 110)))
-        self.logo = Label(image=img) 
-        self.logo.image = img 
-        self.logo.place(x=0, y=0) 
- 
-        separator = ttk.Separator(self, orient='horizontal')
-        separator.place(relx=0, y=75, relwidth=1, relheight=1)
+        self.topbanner = Label(self, width = screen_width, height = 4, bg = "white")
+        self.topbanner.place(x=0, y=0)
 
-        self.label = Label(self, text="File:", font=('Arial', 13))
+        img = ImageTk.PhotoImage(Image.open("Assets/Images/logo.png").resize((220, 66)))
+        self.logo = Label(borderwidth=0, image=img) 
+        self.logo.image = img 
+        self.logo.place(x=100, y=0) 
+
+        self.midbannerone = Label(self, width = screen_width, height = 10, bg = "#12284C")
+        self.midbannerone.place(x=0, y=66)
+
+        self.maintext = Label(self, text="Data Extraction App", fg="white", bg=darkish_blue, font=('Verdana', 40, "bold"))
+        print(screen_width, " ", self.maintext.winfo_width())
+        self.maintext.place(x=(screen_width-self.maintext.winfo_reqwidth())/2, y=105)
+ 
+        # separator = ttk.Separator(self, orient='horizontal')
+        # separator.place(relx=0, y=75, relwidth=1, relheight=1)
+
+        self.midbannertwo = Label(self, width = screen_width, height = 60, bg = "#EBEBEB")
+        self.midbannertwo.place(x=0, y=66+self.midbannerone.winfo_reqheight())
+
+        self.label = Label(self, text="", font=('Arial', 13))
         self.label.pack(pady=((screen_height-280)/2,0))
  
-        self.select_button =Button(self, text="Choose File", fg=darkish_blue, bg="white", font=('Arial', 11), command=self.click_select)
-        self.select_button.pack(pady=10)
+        self.select_button_border = Frame(self, highlightbackground = darkish_blue, 
+                        highlightthickness = 2, bd=0)
+        self.select_button =Button(self.select_button_border, text="Choose File", fg=darkish_blue, 
+                        bg="#EBEBEB", font=('Verdaba', 15), borderwidth=0, command=self.click_select)
+        self.select_button.pack(padx=20, pady=4)
+        self.select_button_border.pack()
 
         self.select_label_text = "No file selected"
 
         self.select_label = Label(self, text=self.select_label_text, font=('Arial', 18))
-        self.select_label.pack(pady=(0,30))
+        self.select_label.pack(pady=30)
 
-        self.upload_button = Button(self, text="Upload", fg=darkish_blue, bg="white", font=('Arial', 18), command=lambda: controller.show_frame(InfoPage))
+        self.upload_button_border = Frame(self, highlightbackground = darkish_blue, 
+                        highlightthickness = 2, bd=0)
+        self.upload_button = Button(self.upload_button_border, text="Upload", fg=darkish_blue, 
+                        bg="#EBEBEB", font=('Verdana', 15), borderwidth=0, command=lambda: controller.show_frame(InfoPage))
 
     def click_select(self):
         f = filedialog.askopenfilename()
         try:
             self.file_text = readfile.ReadFile().exec(f)
             self.select_label.config(text=f, fg='black')
-            self.upload_button.pack()
+            self.upload_button.pack(padx=37, pady=2)
+            self.upload_button_border.pack()
         except ValueError as e:
             self.select_label.config(text="File not supported, pdf only", fg='red')
             self.upload_button.pack_forget()
+            self.upload_button_border.pack_forget()
 
     def get_text(self):
         return self.file_text
@@ -99,16 +121,13 @@ class InfoPage(Frame):
 
         self.mainpage = mainpage
 
-        separator = ttk.Separator(self, orient='horizontal')
-        separator.place(relx=0, y=75, relwidth=1, relheight=1)
-
         self.label = Label(self, text="", font="Arial")
         self.label.pack(pady=(120,0))
 
         self.update_button = Button(self, text="Update", fg="white", bg="#F5333F", font=('Arial', 18), command=self.update_text)
         self.update_button.pack()
 
-        self.back_arrow_photo = ImageTk.PhotoImage(Image.open("src/Assets/Images/Arrow.png").resize((51, 45)))
+        self.back_arrow_photo = ImageTk.PhotoImage(Image.open("Assets/Images/Arrow.png").resize((51, 45)))
         self.back_button = Button(self, image=self.back_arrow_photo, borderwidth=0, command=lambda: controller.show_frame(MainPage))
         self.back_button.place(x=0, y=76)
 
