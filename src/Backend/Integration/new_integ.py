38d414e02c02276a59beb399a051b80dd4264409
tosinfad <90780970+tosinfad@users.noreply.github.com>
2023-02-19 15:20:57 +0000

    data structure for all answers

--- src/Backend/Integration/new_integ.py
+++ src/Backend/Integration/new_integ.py
@@ -21,10 +21,10 @@ class main():
         self.admin_2_codes = self.get_pcodes()[1]
         # self.get_pcodes()
         self.ISO = self.get_ISO_code()
+        self.final_extract=self.extract()
 
 
 
-    
 
     def get_first_page(self):
         # reader = ReadFile()
@@ -37,10 +37,13 @@ class main():
     
     def get_answers(self):
        answers=qaModel(self.file).answers
+       print(answers)
        return answers
+       
     
     def get_admin_0(self):
         country = self.get_answers()["What is the Country of Disaster?"]
+        print(country)
         return country
 
     def get_ISO_code(self):
@@ -67,15 +70,47 @@ class main():
         # print(type(cosine.p_code_1),cosine.p_code_1)
         # print(type(cosine.p_code_2),cosine.p_code_2)
         return([cosine.p_code_1,cosine.p_code_2])
+    
+    def extract(self):
+        final = {}
+        # country
+        final["Country"]=self.admin_0
+        # iso
+        final["ISO"]=self.ISO
+        # admin 1(list of dictionary)
+        final["Admin1"]=self.admin_1_codes
+        # admin 2(list of dictionary)
+        final["Admin1"]=self.admin_2_codes
+        # start date
+        final["Start"]=self.get_answers()["What is the Operation Start Date?"]
+        # end date
+        final["End"]=self.get_answers()["What is the Operation End Date?"]
+        # affected
+        final["Affected"]=self.get_answers()["What is the number of people affected?"]
+        # assisted
+        final["Assisted"]=self.get_answers()["What is the number of people assisted?"]
+        # glide
+        final["Glide"]=self.get_answers()["What is the Glide Number?"]
+        # operation number
+        final["OpNum"]=self.get_answers()["What is the Operation n°?"]
+        # operation budget
+        final["OpBud"]=self.get_answers()["What is the Operation Budget?"]
+        # host national society
+        final["Host"]=self.get_answers()["What is the Host National Society?"]
+
+        return final
+
+
 
 
 # test = main("src/Backend/Integration/testfile.pdf")
 path = "src/Backend/Integration/MDRRW014dfr.pdf"
 test = main(path)
+print(test.final_extract)
 
-# print(test.loc_list)
-print("admin 0: " + test.admin_0)
-print(f"ISO code: {test.ISO}" )
-# test.get_pcodes()
-print(f"admin1 codes: {test.admin_1_codes}")
-print(f"admin2 codes: {test.admin_2_codes}")
+# # print(test.loc_list)
+# print("admin 0: " + test.admin_0)
+# print(f"ISO code: {test.ISO}" )
+# # test.get_pcodes()
+# print(f"admin1 codes: {test.admin_1_codes}")
+# print(f"admin2 codes: {test.admin_2_codes}")
