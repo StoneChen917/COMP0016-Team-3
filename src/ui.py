6c647055352568e74a8e2821361ab280df69777a
StoneChen917 <zcabhc0@ucl.ac.uk>
2023-02-20 15:43:19 +0000

    Update ui.py

--- src/ui.py
+++ src/ui.py
@@ -1,10 +1,10 @@
+import magic
 from pathlib import Path
 from tkinter import *
 from tkinter import messagebox, filedialog, ttk
 from PIL import Image, ImageTk
-from Backend.Integration import readfile
 from tkinterdnd2 import *
-from Backend.Integration import new_integ
+#from Backend.Integration import new_integ
 
 
 
@@ -30,13 +30,13 @@ class UI(Tk):
 
         fronttoback = Frontoback()
 
-        mainpage = MainPage(container, self, fronttoback)
-        self.frames[MainPage] = mainpage
-        mainpage.grid(row=0, column=0, sticky="nsew")
-
-        infopage = InfoPage(container, self)
+        infopage = InfoPage(container, self, fronttoback)
         self.frames[InfoPage] = infopage
         infopage.grid(row=0, column=0, sticky="nsew")
+
+        mainpage = MainPage(container, self, fronttoback, infopage)
+        self.frames[MainPage] = mainpage
+        mainpage.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(MainPage)
 
@@ -55,10 +55,11 @@ class MainPage(Frame):
 
     doctext = " "
     
-    def __init__(self, parent, controller, fronttoback):
+    def __init__(self, parent, controller, fronttoback, infopage):
         Frame.__init__(self,parent)
 
         self.fronttoback = fronttoback
+        self.infopage = infopage
 
         self.screen_width = self.winfo_screenwidth() 
         self.screen_height = self.winfo_screenheight()
@@ -69,6 +70,12 @@ class MainPage(Frame):
         logo_image = ImageTk.PhotoImage(Image.open(Path("src/Assets/Images/logo.png")).resize((220, 66)))
         self.logo = Label(borderwidth=0, image=logo_image) 
         self.logo.image = logo_image
+        self.logo.place(x=100, y=0) 
+
+        self.info_image = ImageTk.PhotoImage(Image.open(Path("src/Assets/Images/info.png")).resize((60, 60)))
+        self.info_button = Button(self, image = self.info_image, borderwidth = 0, bg = "white", command = self.click_info)
+        self.info_button.image = self.info_image
+        self.info_button.place(x = self.screen_width-63, y = 3)
         self.logo.place(x=100, y=0) 
 
         self.midbannerone = Label(self, width = self.screen_width, height = 10, bg = "#12284C")
@@ -103,10 +110,18 @@ class MainPage(Frame):
         self.upload_button = Button(self, image=self.upload_image, borderwidth=0, command=lambda: controller.show_frame(InfoPage))
         self.upload_button.image = self.upload_image
 
+    def click_info(self):
+        messagebox.showinfo(title = "Info", 
+                                message = "1.Upload your file by drag and drop, or select from folders. (pdf only)\n2.Check if the informations are correct and push it to the database.")
+
+    def update_info_page(self):
+        self.fronttoback.extract_answers()
+        self.infopage.update()
+
     def upload_success(self, f):
-        readfile.ReadFile().exec(f)
         file_name = f[f.rfind("/")+1:]
         self.fronttoback.set_file_name(file_name)
+        self.update_info_page()
         self.upload_box.configure(image = self.upload_box_uploaded_image)
         self.upload_box.image = self.upload_box_uploaded_image
         self.select_label.config(text = file_name, fg = 'black')
@@ -126,14 +141,14 @@ class MainPage(Frame):
 
     def click_select(self):
         f = filedialog.askopenfilename()
-        try:
+        if "PDF document" in magic.from_file(f):
             self.upload_success(f)
-        except ValueError as e:
+        else:
             self.upload_failure(f)
 
     def drop_file(self, event):
         f = event.data
-        if event.data.endswith(".pdf"):
+        if "PDF document" in magic.from_file(f):
             self.upload_success(f)
         else:
             self.upload_failure(f)
@@ -150,8 +165,10 @@ class MainPage(Frame):
 
 class InfoPage(Frame):
     
-    def __init__(self, parent, controller):
+    def __init__(self, parent, controller, fronttoback):
         Frame.__init__(self, parent)
+
+        self.fronttoback = fronttoback
 
         self.screen_width = self.winfo_screenwidth() 
         self.screen_height = self.winfo_screenheight() 
@@ -165,46 +182,75 @@ class InfoPage(Frame):
         self.back_arrow_photo = ImageTk.PhotoImage(Image.open(Path("src/Assets/Images/Arrow.png")).resize((50, 50)))
         self.back_button = Button(self, image=self.back_arrow_photo, borderwidth=0, command=lambda: controller.show_frame(MainPage))
         self.back_button.place(x=0, y=76)
+
+        grid_x = (self.screen_width-910)/2
+        grid_y = 150
+        label_width = 310
+        label_height = 31
+        text_width = 600
+
+        self.operation_number_label = Label(self, text = "Operation number", font = ('Verdana', 15), borderwidth = 2, fg = darkish_blue, 
+                                                relief = "solid", anchor = "w")
+        self.operation_number_label.place(x = grid_x, y = grid_y, width = label_width, height = label_height)
+        self.operation_number_text = Text(self, font = ('Verdana', 15), borderwidth = 2, fg = darkish_blue, bg = "white")
+        self.operation_number_text.place(x = grid_x+label_width, y = grid_y, width = text_width, height = label_height)
+
+        self.glide_number_label = Label(self, text = "Glide number", font = ('Verdana', 15), borderwidth = 2, fg = darkish_blue, 
+                                                relief = "solid", anchor = "w")
+        self.glide_number_label.place(x = grid_x, y = grid_y+label_height, width = label_width, height = label_height)
+        self.glide_number_text = Text(self, font = ('Verdana', 15), borderwidth = 2, fg = darkish_blue, bg = light_greyish)
+        self.glide_number_text.place(x = grid_x+label_width, y = grid_y+label_height, width = text_width, height = label_height)
+
+        self.host_national_society_label = Label(self, text = "Host national society", font = ('Verdana', 15), borderwidth = 2, fg = darkish_blue, 
+                                                relief = "solid", anchor = "w")
+        self.host_national_society_label.place(x = grid_x, y = grid_y+label_height*2, width = label_width, height = label_height)
+        self.host_national_society_text = Text(self, font = ('Verdana', 15), borderwidth = 2, fg = darkish_blue, bg = "white")
+        self.host_national_society_text.place(x = grid_x+label_width, y = grid_y+label_height*2, width = text_width, height = label_height)
+
+        self.operation_budget_label = Label(self, text = "Operation budget", font = ('Verdana', 15), borderwidth = 2, fg = darkish_blue, 
+                                                relief = "solid", anchor = "w")
+        self.operation_budget_label.place(x = grid_x, y = grid_y+label_height*3, width = label_width, height = label_height)
+        self.operation_budget_text = Text(self, font = ('Verdana', 15), borderwidth = 2, fg = darkish_blue, bg = light_greyish)
+        self.operation_budget_text.place(x = grid_x+label_width, y = grid_y+label_height*3, width = text_width, height = label_height)
+
+        self.operation_start_date_label = Label(self, text = "Operation start date", font = ('Verdana', 15), borderwidth = 2, fg = darkish_blue, 
+                                                relief = "solid", anchor = "w")
+        self.operation_start_date_label.place(x = grid_x, y = grid_y+label_height*4, width = label_width, height = label_height)
+        self.operation_start_date_text = Text(self, font = ('Verdana', 15), borderwidth = 2, fg = darkish_blue, bg = "white")
+        self.operation_start_date_text.place(x = grid_x+label_width, y = grid_y+label_height*4, width = text_width, height = label_height)
+
+        self.operation_end_date_label = Label(self, text = "Operation end date", font = ('Verdana', 15), borderwidth = 2, fg = darkish_blue, 
+                                                relief = "solid", anchor = "w")
+        self.operation_end_date_label.place(x = grid_x, y = grid_y+label_height*5, width = label_width, height = label_height)
+        self.operation_end_date_text = Text(self, font = ('Verdana', 15), borderwidth = 2, fg = darkish_blue, bg = light_greyish)
+        self.operation_end_date_text.place(x = grid_x+label_width, y = grid_y+label_height*5, width = text_width, height = label_height)
+
+        self.number_of_people_affected_label = Label(self, text = "Number of people affected", font = ('Verdana', 15), borderwidth = 2, fg = darkish_blue, 
+                                                relief = "solid", anchor = "w")
+        self.number_of_people_affected_label.place(x = grid_x, y = grid_y+label_height*6, width = label_width, height = label_height)
+        self.number_of_people_affected_text = Text(self, font = ('Verdana', 15), borderwidth = 2, fg = darkish_blue, bg = "white")
+        self.number_of_people_affected_text.place(x = grid_x+label_width, y = grid_y+label_height*6, width = text_width, height = label_height)
+        
+        self.number_of_people_assisted_label = Label(self, text = "Number of people assisted", font = ('Verdana', 15), borderwidth = 2, fg = darkish_blue, 
+                                                relief = "solid", anchor = "w")
+        self.number_of_people_assisted_label.place(x = grid_x, y = grid_y+label_height*7, width = label_width, height = label_height)
+        self.number_of_people_assisted_text = Text(self, font = ('Verdana', 15), borderwidth = 2, fg = darkish_blue, bg = light_greyish)
+        self.number_of_people_assisted_text.place(x = grid_x+label_width, y = grid_y+label_height*7, width = text_width, height = label_height)
                 
-        admin0 = Frontoback.get_admin0()
-        output = "Admin 0: " + admin0
-        self.label = Label(self, text= output, font="Arial")
-        self.label.pack(pady= 7, padx= 5, side= TOP)
-        
-        admin1 = Frontoback.get_admin1()
-        output = "Admin 1: " + admin1
-        self.label = Label(self, text= output, font="Arial")
-        self.label.pack(pady= 7, padx= 5, side= TOP)
-        
-        admin2 = Frontoback.get_admin2()
-        output = "Admin 2: " + admin2
-        self.label = Label(self, text= output, font="Arial")
-        self.label.pack(pady= 7, padx= 5, side= TOP)
-        
-        start = Frontoback.get_start()
-        output = "Start Date: " + start
-        self.label = Label(self, text= output, font="Arial")
-        self.label.pack(pady= 7, padx= 5, side= TOP)
-        
-        end = Frontoback.get_end()
-        output = "End Date: " + end
-        self.label = Label(self, text= output, font="Arial")
-        self.label.pack(pady= 7, padx= 5, side= TOP)
-        
-        glide = Frontoback.get_glide()
-        output = "Glide Number: " + glide
-        self.label = Label(self, text= output, font="Arial")
-        self.label.pack(pady= 7, padx= 5, side= TOP)
-        
-        operationbudget = Frontoback.get_operationbudget()
-        output = "Operation Budget: " + operationbudget
-        self.label = Label(self, text= output, font="Arial")
-        self.label.pack(pady= 7, padx= 5, side= TOP)
-        
-        host = Frontoback.get_host()
-        output = "Host National Society: " + host
-        self.label = Label(self, text= output, font="Arial")
-        self.label.pack(pady= 7, padx= 5, side= TOP)
+        self.push_image = ImageTk.PhotoImage(Image.open(Path("src/Assets/Images/push_button.png")).resize((190, 60)))
+        self.push_button = Button(self, image = self.push_image, borderwidth = 0, command = self.click_push)
+        self.push_button.image = self.push_image
+        self.push_button.place(x = (self.screen_width-self.push_button.winfo_reqwidth())/2, y = 430)
+
+    def update(self):
+        self.glide_number_text.insert(INSERT, self.fronttoback.get_glide())
+        self.operation_start_date_text.insert(INSERT, self.fronttoback.get_start())
+        self.operation_end_date_text.insert(INSERT, self.fronttoback.get_end())
+        self.operation_budget_text.insert(INSERT, self.fronttoback.get_operationbudget())
+        self.host_national_society_text.insert(INSERT, self.fronttoback.get_host())
+
+    def click_push(self):
+        None
         
                       
    
@@ -213,6 +259,11 @@ class Frontoback():
     def __init__(self):
         self.file_name = ""
         self.answers = []
+
+    def extract_answers(self):
+        pass
+        # extract the answers
+        # self.answers = eg. extract(self.file_name) or sth like that
     
     def set_file_name(self, name):
         self.file_name = name
