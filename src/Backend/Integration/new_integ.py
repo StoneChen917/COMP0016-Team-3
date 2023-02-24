e354f6249af49a13ed731095a2f1ec64132c4d83
zaynbaig <91963906+zaynbaig@users.noreply.github.com>
2023-02-24 15:55:35 +0000

    updated batch integration

--- src/Backend/Integration/new_integ.py
+++ src/Backend/Integration/new_integ.py
@@ -3,10 +3,16 @@
     # make models take in text from here, from class attributes
 
 import PyPDF2
-from readfile import ReadFile
+import pandas as pd
+import openpyxl
+import xlsxwriter
+
+
 from QA_model import qaModel
 from code_matching import codeMatch
 from locations import Locations
+from readfile import ReadFile
+
 
 class main():
     def __init__(self, file):
@@ -96,12 +102,69 @@ class main():
         return final
 
 
+def dict_parser(final,path,file_num):
+   # for item in final.items():
+
+        Country = final['Country']
+        ISO = final['ISO']
+        
+        Admin1 = " "
+        ad1len = len(final['Admin1'])
+        for x in range(ad1len):
+            result = final['Admin1'][x].get('P-Code')
+            Admin1 = result + " "
+        
+        Admin2 = " "
+        ad2len = len(final['Admin2'])
+        for x in range(ad2len):
+            result = final['Admin2'][x].get('P-Code')
+            Admin2 = result + " "
+        
+        Start = final['Start']
+        End = final['End']
+        Affected = final['Affected']
+        Assisted = final['Assisted']
+        Glide = final['Glide']
+        OpNum = final['OpNum']
+        OpBud = final['OpBud']
+        Host = final['Host']
+        print(path, Country, ISO, Admin1, Admin2,Start,End,Affected,Assisted,Glide,OpNum,OpBud,Host)
+        workbook = xlsxwriter.Workbook('src/Backend/Integration/Results_batch.xlsx')
+        worksheet = workbook.add_worksheet("Sheet 1")
+
+        ans = (
+            [path, Country, ISO, Admin1, Admin2,Start,End,Affected,Assisted,Glide,OpNum,OpBud,Host]
+        )
+        row = 0 + file_num
+        col = 0
+
+        for path, Country, ISO, Admin1, Admin2,Start,End,Affected,Assisted,Glide,OpNum,OpBud,Host in (ans):
+            worksheet.write(row, col, path)
+            worksheet.write(row, col + 1, Country)
+            worksheet.write(row, col + 2, ISO)
+            worksheet.write(row, col + 3, Admin1)
+            worksheet.write(row, col + 4, Admin2)
+            worksheet.write(row, col + 5, Start)
+            worksheet.write(row, col + 6, End)
+            worksheet.write(row, col + 7, Affected)
+            worksheet.write(row, col + 8, Assisted)
+            worksheet.write(row, col + 9, Glide)
+            worksheet.write(row, col + 10, OpNum)
+            worksheet.write(row, col + 11, OpBud)
+            worksheet.write(row, col + 12, Host)
+
+        workbook.close()
+
 
 
 # test = main("src/Backend/Integration/testfile.pdf")
-# path = "src/Backend/Integration/MDRRW014dfr.pdf"
-# test = main(path)
-# print(test.final_extract)
+path = "src/Backend/Integration/MDRRW014dfr.pdf"#edit
+test = main(path)
+file_num = 1#edit
+#print(test.final_extract)
+#dict_parser(test.final_extract,path,file_num)
+test_dict = {'Country': 'Rwanda', 'ISO': 'RWA', 'Admin1': [{'Location': 'Eastern Province', 'P-Code': '20RWA005'}, {'Location': 'the City of Kigali', 'P-Code': '20RWA001'}], 'Admin2': [{'Location': 'Flanders', 'P-Code': '20RWA004042'}, {'Location': 'Gatsibo district', 'P-Code': '20RWA005053'}, {'Location': 'Gatsibo District', 'P-Code': '20RWA005053'}, {'Location': 'Gatsibo d istrict \n©IFRC', 'P-Code': '20R053WA005053'}], 'Start': '11 July 2017', 'End': '01 September 2017', 'Affected': '675', 'Assisted': '811 households', 'Glide': 'ST-2017 -000035 -RWA', 'OpNum': 'MDRRW014', 'OpBud': 'CHF 49,122', 'Host': 'Rwanda Red Cross Society'}
+dict_parser(test_dict,path,file_num)
 
 # # print(test.loc_list)
 # print("admin 0: " + test.admin_0)
