98f21e62569b5bbb6549a6396bec4a62f587345f
zaynbaig21 <119931732+zaynbaig21@users.noreply.github.com>
2023-03-15 19:00:36 +0000

    testing

---
+++ src/Backend/UnitTesting/test_fronttoback.py
@@ -0,0 +1,63 @@
+from unittest import TestCase
+
+
+class TestFrontoback(TestCase):
+    def test_extract_answers(self):
+        self.fail()
+
+    def test_reset_ftb(self):
+        self.fail()
+
+    def test_is_finished(self):
+        self.fail()
+
+    def test_is_finished(self):
+        self.fail()
+
+    def test_get_files(self):
+        self.fail()
+
+    def test_get_answers(self):
+        self.fail()
+
+    def test_get_max(self):
+        self.fail()
+
+    def test_set_files(self):
+        self.fail()
+
+    def test_get_admin0(self):
+        self.fail()
+
+    def test_get_iso(self):
+        self.fail()
+
+    def test_get_admin1(self):
+        self.fail()
+
+    def test_get_admin2(self):
+        self.fail()
+
+    def test_get_start(self):
+        self.fail()
+
+    def test_get_end(self):
+        self.fail()
+
+    def test_get_glide(self):
+        self.fail()
+
+    def test_get_operation_number(self):
+        self.fail()
+
+    def test_get_operation_budget(self):
+        self.fail()
+
+    def test_get_host(self):
+        self.fail()
+
+    def test_get_affected(self):
+        self.fail()
+
+    def test_get_assisted(self):
+        self.fail()
---
+++ src/Backend/UnitTesting/test_readfile.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestReadFile(TestCase):
+    def test_exec(self):
+        self.fail()
+
+    def test_check_type(self):
+        self.fail()
+
+    def test_read_file(self):
+        self.fail()
