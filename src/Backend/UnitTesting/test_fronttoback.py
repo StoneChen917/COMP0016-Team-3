0c1c1c8c9d20adbc0738eda878cbb232e104bfbe
zaynbaig21 <119931732+zaynbaig21@users.noreply.github.com>
2023-03-16 15:42:04 +0000

    unit tests

--- src/Backend/UnitTesting/test_fronttoback.py
+++ src/Backend/UnitTesting/test_fronttoback.py
@@ -1,63 +1,83 @@
 from unittest import TestCase
-
+from Integration import fronttoback
 
 class TestFrontoback(TestCase):
+
+    def setUp(self):
+        self.files = ['MDRRW014dfr']
+        self.finished = False
+        self.max = 0
+        self.answers = [{"Country": "Rwanda", "ISO": "RWA", "Admin1": "20RWA001 ", "Admin2": "20R053WA005053 ", "Start": "11 July 2017", 
+             "End": "01 September 2017", "Glide": "ST-2017 -000035 -RWA", "OpNum": "MDRRW014", "OpBud": "CHF 49,122", 
+             "Host": "Rwanda Red Cross Society", "Affected": 675, "Assisted": "811 households"}]
+
     def test_extract_answers(self):
-        self.fail()
+        self.assertEqual(len(self.answers), self.max + 1)
+        self.assertTrue(self.finished)
 
     def test_reset_ftb(self):
-        self.fail()
-
-    def test_is_finished(self):
-        self.fail()
+        self.assertFalse(self.fb.finished)
 
     def test_is_finished(self):
         self.fail()
 
     def test_get_files(self):
-        self.fail()
+        return self.files
 
     def test_get_answers(self):
-        self.fail()
-
+        return self.answers
+    
     def test_get_max(self):
-        self.fail()
+        return self.max
 
     def test_set_files(self):
-        self.fail()
+        for file in self.files:
+            self.files.append(file)
 
     def test_get_admin0(self):
-        self.fail()
+        self.test_extract_answers()
+        self.assertEqual(self.test_get_admin0(1), "Rwanda")
 
     def test_get_iso(self):
-        self.fail()
+        self.test_extract_answers()
+        self.assertEqual(self.test_get_iso(1), "RWA")
 
     def test_get_admin1(self):
-        self.fail()
+        self.test_extract_answers()
+        self.assertEqual(self.test_get_admin1(1), "20RWA001")
 
     def test_get_admin2(self):
-        self.fail()
+        self.test_extract_answers()
+        self.assertEqual(self.test_get_admin2(1), "20R053WA005053")
 
     def test_get_start(self):
-        self.fail()
+        self.test_extract_answers()
+        self.assertEqual(self.test_get_start(1), "11 July 2017")
 
     def test_get_end(self):
-        self.fail()
+        self.test_extract_answers()
+        self.assertEqual(self.test_get_end(1), "01 September 2017")
 
     def test_get_glide(self):
-        self.fail()
+        self.test_extract_answers()
+        self.assertEqual(self.test_get_glide(1), "ST-2017 -000035 -RWA")
 
     def test_get_operation_number(self):
-        self.fail()
+        self.test_extract_answers()
+        self.assertEqual(self.test_get_operation_number(1), "MDRRW014")
 
     def test_get_operation_budget(self):
-        self.fail()
+        self.test_extract_answers()
+        self.assertEqual(self.test_get_operation_budget(1), "CHF 49,122")
 
     def test_get_host(self):
-        self.fail()
+        self.test_extract_answers()
+        self.assertEqual(self.test_get_host(1), "Rwanda Red Cross Society")
 
     def test_get_affected(self):
-        self.fail()
+        self.test_extract_answers()
+        self.assertEqual(self.test_get_affected(1), "675")
 
     def test_get_assisted(self):
-        self.fail()
+        self.test_extract_answers()
+        self.assertEqual(self.test_get_assisted(1), "811 households")
--- src/Backend/UnitTesting/test_readfile.py
+++ src/Backend/UnitTesting/test_readfile.py
@@ -1,12 +1,26 @@
 from unittest import TestCase
-
+from Integration import read_file
+from UnitTesting import *
+import magic
+import PyPDF2
+from PyPDF2 import PdfReader
 
 class TestReadFile(TestCase):
-    def test_exec(self):
-        self.fail()
+
+    def setUp(self):
+        file = "MDRRW014dfr.py"
+        self.check_type(file)
+        return self.test_read_file(file)
 
     def test_check_type(self):
-        self.fail()
+        if ("PDF document" in magic.from_file(self.file)):
+            return True
+        else:
+            raise ValueError("wrong type of file, pdf only")
 
     def test_read_file(self):
-        self.fail()
+        test = read_file(self, self.file)
+        self.assertEqual(self.test_read_file(), "Emergency Plan of Action Final Report Rwanda: Storm and Heavy Winds DREF operation Operation n° MDRRW014 Date of Issue: Glide number: ST-2017-000035-RWA Date of disaster: 01 April 2017 Operation start date: 11 July 2017 Operation end date: 01 September 2017 Host National Society: Rwanda Red Cross Society Operation budget: CHF 49,122 Number of people affected: 675 people (135 households) Number of people assisted: 811 households N° of National Societies involved in the operation: Belgian Red Cross Flanders, Austrian Red Cross, Belgian Red Cross French Community, Spanish Red Cross International Federation of Red Cross and Red Crescent Societies. N° of other partner organizations involved in the operation: Local Authorities, the Ministry of Disaster Management and Refugees Affairs (MIDIMAR) A. Situation analysis Description of the disaster On 01 April 2017 at 3:00 pm, Gatsibo district located in the Eastern Province of Rwanda experienced heavy rainfall associated with heavy storms, which resulted in destruction of houses and community farm lands in Kiramuruzi Sector Nyabisindu Cell. The affected area is located 36 kilometers from Gatsibo District, 40 kilometers from the Eastern Province office and 70 kilometers from the City of Kigali. The preliminary assessment gathered by Rwanda Red Cross staff and volunteers, estimated that 675 people (135 households) were affected by heavy wind and storms. Renewed assessments confirmed that the amount of people affected remained around 135 households. The last performed assessment in June, showed that households were impacted in the following way: • Households with houses destroyed and living with host communities (N= 34 households) • Households living in their destroyed houses (with half a roof) (N= 57 households) • Households that have recovered their houses and are managing (N= 44 households) • Households that need roofing for latrines (N= 811 households) House with roof destroyed in Gatsibo district ©IFRC ")
+
+            
+
